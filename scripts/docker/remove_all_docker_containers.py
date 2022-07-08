85d516ace9610858952dd83511275541f109728e
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-08 22:38:13 +0900

    feat(docker): add `ask_user` function
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- scripts/docker/remove_all_docker_containers.py
+++ scripts/docker/remove_all_docker_containers.py
@@ -2,6 +2,19 @@ import sys
 import subprocess
 
 
+def ask_user(question):
+    while True:
+        print(question)
+        answer = input("Continue? (y/N)")
+        if any(answer.lower() == r for r in ['y']):
+            remove_all_docker_containers()
+            break
+        elif any(answer.lower() == r for r in ['n', '']):
+            break
+        else:
+            print("Please enter 'y' or 'n' ")
+
+
 def remove_all_docker_containers():
     print("Removing all docker containers.")
     try:
@@ -30,4 +43,4 @@ def remove_all_docker_containers():
         sys.exit()
 
 
-remove_all_docker_containers()
+ask_user("All of your docker containers on your host will be removed.")
