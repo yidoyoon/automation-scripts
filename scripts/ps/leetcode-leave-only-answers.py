e7d5fc2efe3bf088c1655f9ea6b7519590717f11
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-17 19:44:08 +0900

    fix(leetcode): fix `trim_solution` function
    Changed the trimming algorithm to fix some files not trimmed normally.
    
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- scripts/ps/leetcode-leave-only-answers.py
+++ scripts/ps/leetcode-leave-only-answers.py
@@ -1,31 +1,40 @@
+import shutil
+import tempfile
+
 from pathlib2 import Path
 
-begin = "//leetcode submit region begin(Prohibit modification and deletion)"
-end = "//leetcode submit region end(Prohibit modification and deletion)"
+"""Remove automatically generated annotations when you use
+`Leetcode Editor` plugin(https://github.com/shuzijun/leetcode-editor)
+in Jetbrains IDE. Only the code(solution) part will be left.
+"""
 
 
-def trim_solution(file_name) -> None:
-    """Remove automatically generated annotations when you use
-    `Leetcode Editor` plugin(https://github.com/shuzijun/leetcode-editor)
-    in Jetbrains IDE. It deletes the first line including the top comment,
-    and then deletes it to the end including the bottom comment. In the end,
-    only the complete code(solution) part is left.
-    """
+def find_range(file_name):
+    begin_string = "//leetcode submit region begin(Prohibit modification and deletion)"
+    end_string = "//leetcode submit region end(Prohibit modification and deletion)"
+    idxs = []
     with open(file_name, 'r') as fr:
-        flag = False
         lines = fr.readlines()
-        with open(file_name, 'w') as fw:
-            for line in lines:
-                if line.strip('\n') == begin:
-                    flag = True
-                    continue
-                if line.strip('\n') == end:
-                    flag = False
-                if flag:
-                    fw.write(line)
-    print(f"{file_name} is ready to commit")
+        for i, line in enumerate(lines):
+            if line.strip('\n') == begin_string:
+                idxs.append(i)
+            if line.strip('\n') == end_string:
+                idxs.append(i)
+
+    return idxs
+
+
+def trim_solution(file_name, begin_idx, end_idx):
+    with tempfile.TemporaryFile(mode='w+', delete=False) as fw, open(file_name, 'r') as fr:
+        lines = fr.readlines()[begin_idx + 1:end_idx]
+        fw.writelines(lines)
+    shutil.move(fw.name, file_name)
 
 
 if __name__ == '__main__':
     for path in Path.cwd().rglob('*.cpp'):
-        trim_solution(path)
+        idxs = find_range(path)
+        try:
+            trim_solution(path, idxs[0], idxs[1])
+        except IndexError as e:
+            pass
